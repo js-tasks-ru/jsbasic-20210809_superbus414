c26ca44afe9d9f35996629c53c1883bf9eb54af9
Artem Khvatov <superbus414@gmail.com>
2021-08-18 13:09:59 +0300

    jsbasic-20210809 Хватов А. Занятие 1. Задача 5. Занятие 2. Задачи 1 - 3.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sumSalaries = 0;
+  for (let key in salaries){
+    if (typeof salaries[key] == 'number' && !Number.isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity){
+    sumSalaries += salaries[key];}
+  }
+  return sumSalaries;
 }
